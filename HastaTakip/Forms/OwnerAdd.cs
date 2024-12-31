201f97f63bcb9b72f7efe9e5f49e74872a54b46e
Muhammed Ali Kutbay <muhammedalikutbay@gmail.com>
2025-01-01 00:31:29 +0300

    id eror fix

--- HastaTakip/Forms/OwnerAdd.cs
+++ HastaTakip/Forms/OwnerAdd.cs
@@ -42,7 +42,5 @@ namespace HastaTakip.Forms
                 }
             }
         }
-
-
     }
 }
--- HastaTakip/Forms/PatientsAdd.cs
+++ HastaTakip/Forms/PatientsAdd.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SQLite;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using HastaTakip.Models;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace HastaTakip.Forms
 {
@@ -21,34 +14,6 @@ namespace HastaTakip.Forms
         {
             InitializeComponent();
             LoadOwners();
-        }
-
-        private void Btn_Add_Click(object sender, EventArgs e)
-        {
-            Patient patient = new Patient()
-            {
-                PatientName = Txt_Name.Text,
-                OwnerId = ownerid,
-                PatientGender = Txt_Gender.Text,
-                RegistrationDate = DateTime.Now.ToString("dd-MM-yyyy"),
-                PatientNote = Txt_Note.Text,
-            };
-
-            using (SQLiteConnection conn = new SQLiteConnection("Data Source=pf55.db;Version=3;"))
-            {
-                conn.Open();
-
-                string query =
-                    $"INSERT INTO Patients (PatientName,OwnerId,RegistrationDate,PatientGender,PatientNote) VALUES ('{patient.PatientName}',{patient.OwnerId},'{patient.RegistrationDate}','{patient.PatientGender}','{patient.PatientNote}')";
-                using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
-                {
-                    cmd.ExecuteNonQuery();
-                    Clients clients = new Clients();
-                    MessageBox.Show("Veri başarıyla eklendi!");
-                    clients.LoadData();
-                    this.Close();
-                }
-            }
         }
 
         private void LoadOwners()
@@ -82,6 +47,34 @@ namespace HastaTakip.Forms
             OwnerAdd ownerAdd = new OwnerAdd();
             ownerAdd.TopMost = true;
             ownerAdd.Show();
+        }
+
+        private void Btn_Add_Click(object sender, EventArgs e)
+        {
+            Patient patient = new Patient()
+            {
+                PatientName = Txt_Name.Text,
+                OwnerId = ownerid,
+                PatientGender = Txt_Gender.Text,
+                RegistrationDate = DateTime.Now.ToString("dd-MM-yyyy"),
+                PatientNote = Txt_Note.Text,
+            };
+
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=pf55.db;Version=3;"))
+            {
+                conn.Open();
+
+                string query =
+                    $"INSERT INTO Patients (PatientName,OwnerId,RegistrationDate,PatientGender,PatientNote) VALUES ('{patient.PatientName}',{patient.OwnerId},'{patient.RegistrationDate}','{patient.PatientGender}','{patient.PatientNote}')";
+                using (SQLiteCommand cmd = new SQLiteCommand(query, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                    Clients clients = new Clients();
+                    MessageBox.Show("Veri başarıyla eklendi!");
+                    clients.LoadData();
+                    this.Close();
+                }
+            }
         }
     }
 }
